54ebb628203bccda484cc148ec1b0e9726b0db12
Mariano Cardoso <marianocardoso92@gmail.com>
2024-09-21 01:55:58 -0300

    Primera Subida

--- ReemplazoCaracteres/Program.cs
+++ ReemplazoCaracteres/Program.cs
@@ -13,42 +13,6 @@ namespace EJERCICIO3
             //CARÁCTER 1: ‘a’ CARÁCTER 2: ‘i’
             //CADENA RESULTADO: “Li mir estibi sereni"
 
-            // char[] frase = new char[30];
-            // char c1, c2, letra;
-            // int i = 0;
-
-            // Console.WriteLine("Ingrese un caracter: ");
-            // c1 = char.Parse(Console.ReadLine());
-
-            // Console.WriteLine("Ingrese otro caracter: ");
-            // c2 = char.Parse(Console.ReadLine());
-            
-            // Console.WriteLine("Ingrese una letra: ");
-            // letra = char.Parse(Console.ReadLine());
-            // while (letra != '0' && i < 30)
-            // {
-            //     frase[i] = letra;
-            //     Console.WriteLine("Ingrese otra letra: ");
-            //     letra = char.Parse(Console.ReadLine());
-            //     i++;
-            // }
-            // frase[i] = '\0';
-            // i = 0;
-
-            // while(frase[i] != '\0')
-            // {
-            //     if(frase[i] == c1)
-            //         frase[i] = c2;
-            //     i++;
-            // }
-            // i = 0;
-
-            // while(frase[i] != '\0')
-            // {
-            //     Console.Write(frase[i]);
-            //     i++;
-            // }
-
             string frase;
             char letraActual;
             char letraNueva;
